8424609c7acfdb1e6ce43611a47a37dfd8cd38a6
wslyvh <wslyvh@users.noreply.github.com>
2020-11-18 11:11:28 +0100

    cms

---
+++ gatsby-config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  siteMetadata: {
+    title: `Devcon 6`,
+    description: `The annual conference for all Ethereum developers, researchers, thinkers, and makers.`,
+  },
+  plugins: [
+    'gatsby-plugin-root-import',
+    'gatsby-plugin-typescript',
+    'gatsby-plugin-netlify-cms',
+  ],
+}
